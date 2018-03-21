14bb26b2070e83d783426005658866c4188bcc6d
Nikolay Vichev <nikolay.vichev196@gmail.com>
2018-03-21 19:41:53 +0200

    Solved Problems 1 and 2 from Assignment 1.

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,33 @@
+#a)
+x <- c(4,1,1,4)
+x
 
+
+#b)
+y <- c(1,4)
+y
+
+
+#c) The 2 vectors have different lengths, so the length of the shorter 
+#vector is doubled to match the 4 elements of the bigger one. 
+x-y
+
+#d)
+s <- c(x,y)
+s
+
+#e)
+sReplicated <- rep(s,10)
+length(sReplicated)
+
+#f)
+sRep_Each <- rep(s,each = 3)
+sRep_Each
+
+#g)
+seq1 <- seq(7,21)
+seq1
+7:21
+
+#h)
+length(seq1)
--- Problem_2.R
+++ Problem_2.R
@@ -1,1 +1,73 @@
+#a)
+xmin <- c(23, 20.5, 28.2, 20.3, 22.4, 17.2, 18.2)
+xmax <- c(25, 22.8, 31.2, 27.3, 28.4, 20.2, 24.1)
+xmin
+xmax
 
+
+#b)
+amplitude <- xmax - xmin
+amplitude
+
+#c) 
+avgMin <- mean(xmin)
+avgMin
+
+avgMax <- mean(xmax)
+avgMax
+
+
+#d)
+xmin[xmin < avgMin]
+
+
+#e)
+xmax[xmax > avgMax]
+
+
+#f)
+dayNames <- c('Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun')
+dayNames
+
+
+names(xmin) <- dayNames
+xmin
+
+names(xmax) <- dayNames
+xmax
+
+
+#g)
+temperatures <- data.frame(
+  xmin = xmin,
+  xmax = xmax)
+temperatures
+
+
+#h) 
+temperatures <- within(temperatures, {
+  xMinFahrenheit <- xmin*9/5 + 32})
+temperatures
+
+
+#i)
+temperaturesFahr <- data.frame(xminFahr = xmin*9/5 + 32,
+                            xmaxFahr = xmax*9/5 + 32)
+temperaturesFahr
+
+
+#j)
+TempWorkDays <- data.frame(
+  xminFahr = xmin[seq(1:5)]*9/5 + 32,
+  xmaxFahr = xmax[seq(1:5)]*9/5 + 32
+)
+TempWorkDays
+
+
+
+
+TempWorkDays2 <- data.frame(
+  xminFahr = xmin[seq(length(xmin)-2)]*9/5 + 32,
+  xmaxFahr = xmax[seq(length(xmax)-2)]*9/5 + 32
+)
+TempWorkDays2
